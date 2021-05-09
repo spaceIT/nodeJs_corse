e1b1fd3cdf5246e2e6aa13e4f56f171580849acc
Andrey <andrei.svistun@awtg.co.uk>
2021-05-09 13:50:16 +0300

    add readable input value

--- Streams_module.js
+++ Streams_module.js
@@ -3,6 +3,14 @@ const fs = require('fs');
 const Crypt_module = require('./Crypt_module');
 
 const getReadableStream = (inputValue) => {
+    if (inputValue === undefined) {
+        process.stdin.setEncoding('utf8');
+
+        return process.stdin.on('readable', () => {
+            process.stdin.read();
+        });
+    }
+
     const urlPath = `./${inputValue}`;
     const readStream = fs.createReadStream(urlPath, 'utf8');
 
